66ae8cc8702d723249be78ae97d0d3ad1609a85e
BuildTools <unconfigured@null.spigotmc.org>
2018-02-27 19:51:45 +0900

    Initial commit.

---
+++ js/index.js
@@ -0,0 +1,35 @@
+---
+---
+var page = 1;
+
+$(document).ready(function() {
+  $.ajax({
+    url: '{{ site.baseurl }}/write',
+    success: function(result) {
+      $('#content').append(result)
+    }
+  })
+  
+  $.ajax({
+    url: '{{ site.baseurl }}/page',
+    success: function(result) {
+      $('#content').append(result)
+    }
+  })
+  
+  $('#loadmore').click(function() {
+    page++;
+    $.ajax({
+      url: '{{ site.baseurl }}/page/' + page + '/',
+      success: function(result) {
+        $('#content').append(result)
+      },
+      error: function(xhr, status, error) {
+        if(xhr.status == 404) {
+          $('#loadmore').html('No More')
+        }
+      }
+    })
+  })
+})
+
---
+++ js/write.js
@@ -0,0 +1,49 @@
+Date.prototype.format = function(f) {
+    if (!this.valueOf()) return " ";
+ 
+    var weekName = ["일요일", "월요일", "화요일", "수요일", "목요일", "금요일", "토요일"];
+    var d = this;
+     
+    return f.replace(/(yyyy|yy|MM|dd|E|hh|mm|ss|a\/p)/gi, function($1) {
+        switch ($1) {
+            case "yyyy": return d.getFullYear();
+            case "yy": return (d.getFullYear() % 1000).zf(2);
+            case "MM": return (d.getMonth() + 1).zf(2);
+            case "dd": return d.getDate().zf(2);
+            case "E": return weekName[d.getDay()];
+            case "HH": return d.getHours().zf(2);
+            case "hh": return ((h = d.getHours() % 12) ? h : 12).zf(2);
+            case "mm": return d.getMinutes().zf(2);
+            case "ss": return d.getSeconds().zf(2);
+            case "a/p": return d.getHours() < 12 ? "오전" : "오후";
+            default: return $1;
+        }
+    });
+};
+String.prototype.string = function(len){var s = '', i = 0; while (i++ < len) { s += this; } return s;};
+String.prototype.zf = function(len){return "0".string(len - this.length) + this;};
+Number.prototype.zf = function(len){return this.toString().zf(len);};
+
+$(document).ready(function() {
+  $('#submit').click(function() {
+    const username = $('#username')[0].value
+    const repo = 'GitHuber'
+    const branch = 'master'
+    const message = $('#message')[0].value
+    const date = new Date().format("yyyy-MM-dd-HH-mm-ss")
+    $.ajax({
+      type: 'POST',
+      url: 'https://api.staticman.net/v2/entry/' + username + '/' + repo + '/' + branch + '/write',
+      data: {
+        fields: {
+          name: username,
+          message: message,
+          date: date
+        }
+      },
+      success: function(result) {
+        console.log(result)
+      }
+    })
+  })
+})
